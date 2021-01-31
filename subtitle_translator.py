08281b8d95760f27dabd13217de9248042d8954c
morteza <dr-henry@hotmail.com>
2021-01-31 17:52:32 +0330

    small changes

--- subtitle_translator.py
+++ subtitle_translator.py
@@ -3,7 +3,7 @@ import time
 from utils import translate_and_compose
 import re
 
-for root, dirs, files in os.walk("."):
+for root, dirs, files in os.walk("./put_files_here"):
     for name in files:
         if re.match(r".*(?<!-\w{2})\.srt$", name):
             folder = os.path.abspath(root)
@@ -12,12 +12,12 @@ for root, dirs, files in os.walk("."):
             if os.path.isfile(dest_file):
                 print(f'"{name}" translated before')
             else:
-                print(f"translating {name}")
+                print(f'translating "{name}"')
                 file_must_update = False
                 with open(source_file, 'r') as srt_file:
                     srt_content = srt_file.read()
                     # remove extra numbers
-                    new_srt_content = re.sub(r"(\d+)(\n+)(\d+\n+)", r"\n\1\n", srt_content)
+                    new_srt_content = re.sub(r"(^|\n)(\d+)(\n+)(\d+\n+)", r"\n\2\n", srt_content)
 
                     # remove newLine from file beginning
                     new_srt_content = re.sub(r"^\n+", "", new_srt_content)
@@ -25,6 +25,7 @@ for root, dirs, files in os.walk("."):
                     # replace &amp;
                     new_srt_content = new_srt_content.replace("&amp;", "&")
                 if srt_content is not new_srt_content:
+                    os.rename(source_file, source_file + ".backup")
                     with open(source_file, 'w') as srt_file_w:
                         srt_file_w.write(new_srt_content)
                 translate_and_compose(source_file, dest_file, 'en', 'fa', mode='naive', both=False)
